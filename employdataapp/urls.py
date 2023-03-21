7965d14b49771a504abd2f571579e66290c395b0
DivyaHarishKathu <divya.hk9520@gmail.com>
2023-03-21 12:59:57 +0530

    first commit

---
+++ employdataapp/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+from django.urls import path
+
+urlpatterns=[
+    path('',views.index, name="index"),
+    path('view_all_emp',views.view_all_emp, name='view_all_emp'),
+    path('add_emp', views.add_emp, name="add_emp"),
+    path('remove_emp', views.remove_emp, name="remove_emp"),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name="remove_emp"),
+    path('filter_emp', views.filter_emp, name="filter_emp"),
+]
